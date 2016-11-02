0e5b24faa68cad9e119434a26cd249923e295b1c
chohner <mail@chohner.com>
2016-11-02 18:37:52 +0100

    rename&move to script folder
    also: echo raspberry IP

--- scripts/rasp_IP.sh
+++ scripts/rasp_IP.sh
@@ -1,4 +1,4 @@
 ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 echo Your IP: $ip
-sudo nmap -sP $ip/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'
+sudo nmap -sP $ip/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print "Raspberry IP: "ip}'
 
