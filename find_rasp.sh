fce918da87e7bea1d08a61a57f13ab815281baa9
Chris Hold <Christoph.Hold@Telekom.de>
2016-11-02 18:41:57 +0100

    Sweep in IP space

--- find_rasp.sh
+++ find_rasp.sh
@@ -1,4 +1,13 @@
-ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-echo Your IP: $ip
-sudo nmap -sP $ip/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'
-
+ip_host=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+echo Host IP: $ip_host
+# first two bytes
+ip_static=${ip_host:0:8}
+# third byte
+ip_sweep=${ip_host:8:2}
+# search range around third byte
+r=1
+for ((i=-r; i<=r; i++)); do
+  s=$ip_static$((ip_sweep+i)).0/24
+  echo "Searching in : "$s
+  sudo nmap -sP $s | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'
+done
