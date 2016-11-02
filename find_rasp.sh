74cf633810ea4952706fc5b9414615e2953ea160
chohner <mail@chohner.com>
2016-11-02 17:30:01 +0100

    add script to find rasp IP

---
+++ find_rasp.sh
@@ -0,0 +1,3 @@
+ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+sudo nmap -sP $ip/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'
+
