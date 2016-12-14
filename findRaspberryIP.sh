a6cf45d1f6edd964850523c09e0d4d451b6db845
Chris Hold <chris.hold@gmx.de>
2016-12-14 20:11:07 +0100

    Clear period in IP

--- findRaspberryIP.sh
+++ findRaspberryIP.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+
 ip_host=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 echo Host IP: $ip_host
 # first two bytes
 ip_static=${ip_host:0:8}
 # third byte
 ip_sweep=${ip_host:8:2}
+
+# clear period
+if [ "${ip_sweep:1}" = "." ]
+  then
+    ip_sweep=${ip_sweep:0:1}
+fi
+
 # search range around third byte
 r=1
 for ((i=-r; i<=r; i++)); do
@@ -11,4 +20,3 @@ for ((i=-r; i<=r; i++)); do
   echo "Searching in : "$s
   sudo nmap -sP $s | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print "Raspberry IP: "ip}'
 done
-
