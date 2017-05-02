1d64f4dc7e4af1bac79fa40177d7fe140a31a3db
Chris Hold <Christoph.Hold@Telekom.de>
2017-05-02 16:03:19 +0200

    Clean up and beautify

--- src/potis/potis.cpp
+++ src/potis/potis.cpp
@@ -26,153 +26,149 @@ uint8_t writeBuf[3];
 uint8_t readBuf[2];
 
 void setup_ads(){
-  //open device on /dev/i2c-1
-  if ((fd = open("/dev/i2c-1", O_RDWR)) < 0) {
-    cout << "Error: Couldn't open device! " << fd << endl;
-    exit (1);
-  }
+        //open device on /dev/i2c-1
+        if ((fd = open("/dev/i2c-1", O_RDWR)) < 0) {
+                cout << "Error: Couldn't open device! " << fd << endl;
+                exit (1);
+        }
 
-  // connect to ads1115 as i2c slave
-  if (ioctl(fd, I2C_SLAVE, ads_address) < 0) {
-    cout << "Error: Couldn't find device on address!" << endl;
-    exit (1);
-  }
+        // connect to ads1115 as i2c slave
+        if (ioctl(fd, I2C_SLAVE, ads_address) < 0) {
+                cout << "Error: Couldn't find device on address!" << endl;
+                exit (1);
+        }
 
-  bool continuousMode = true;
+        bool continuousMode = true;
 
-  // setup ADS1115
-  writeBuf[0] = 0b00000001; // (points to Config register)
-  writeBuf[1] = 0b11000101; // bit 15-8 0xD3
-  writeBuf[2] = 0b10000011; // bits 7-0  0x85
-  if (continuousMode)
-    writeBuf[1] = 0b11000100;
+        // setup ADS1115
+        writeBuf[0] = 0b00000001; // (points to Config register)
+        writeBuf[1] = 0b11000101; // bit 15-8 0xD3
+        writeBuf[2] = 0b10000011; // bits 7-0  0x85
+        if (continuousMode)
+                writeBuf[1] = 0b11000100;
 
+        VPS = 2.048 / 32768.0; // volts per step
 
-  VPS = 2.048 / 32768.0; // volts per step
+        // begin conversation
+        // track setup time
+        std::chrono::high_resolution_clock::time_point start, end;
+        start = std::chrono::high_resolution_clock::now();
 
-  // begin conversation
-  // track setup time
-  std::chrono::high_resolution_clock::time_point start, end;
-  start = std::chrono::high_resolution_clock::now();
+        if (write(fd, writeBuf, 3) != 3) {
+                perror("Write to register 1");
+                exit(-1);
+        }
 
-  if (write(fd, writeBuf, 3) != 3) {
-    perror("Write to register 1");
-    exit(-1);
-  }
+        if (!continuousMode) {
+                // wait for bit 15 (1 : ready)
+                while ((writeBuf[0] & 0x80) == 0) {
+                        if (read(fd, writeBuf, 2) != 2) {
+                                perror("Read conversion");
+                                exit(-1);
+                        }
+                }
+        } else {
+                sleep(1);
+        }
 
-  if (!continuousMode){
-    // wait for bit 15 (1 : ready)
-    while ((writeBuf[0] & 0x80) == 0) {
-      if (read(fd, writeBuf, 2) != 2) {
-        perror("Read conversion");
-        exit(-1);
-      }
-    }
-  } else {
-    sleep(1);
-  }
+        // set pointer to 0
+        readBuf[0] = 0;
+        if (write(fd, readBuf, 1) != 1) {
+                perror("Write register select");
+                exit(-1);
+        }
 
-  // set pointer to 0
-  readBuf[0] = 0;
-  if (write(fd, readBuf, 1) != 1) {
-    perror("Write register select");
-    exit(-1);
-  }
-
-  cout << "ADS1115 Setup complete" << endl;
-  end = std::chrono::high_resolution_clock::now();
-  double time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>( end - start ).count();
-  cout << "Setup took " << time_elapsed << " miliseconds to execute\n"<< endl;
+        cout << "ADS1115 Setup complete" << endl;
+        end = std::chrono::high_resolution_clock::now();
+        double time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>( end - start ).count();
+        cout << "Setup took " << time_elapsed << " miliseconds to execute\n"<< endl;
 }
 
 void powerdown_ads(){
-  // power down ASD1115
-  writeBuf[0] = 1;    // config register is 1
-  writeBuf[1] = 0b11000101; // bit 15-8 0xC3 single shot on
-  writeBuf[2] = 0b10000011; // bits 7-0  0x85
-  if (write(fd, writeBuf, 3) != 3) {
-    perror("Write to register 1");
-    exit (1);
-  }
-  close(fd);
-  cout << "ADS1115 Powerdown complete\n";
+        // power down ASD1115
+        writeBuf[0] = 1; // config register is 1
+        writeBuf[1] = 0b11000101; // bit 15-8 0xC3 single shot on
+        writeBuf[2] = 0b10000011; // bits 7-0  0x85
+        if (write(fd, writeBuf, 3) != 3) {
+                perror("Write to register 1");
+                exit (1);
+        }
+        close(fd);
+        cout << "ADS1115 Powerdown complete\n";
 }
 
 float read_ads(){
-  // read conversion register
-  if (read(fd, readBuf, 2) != 2) {
-    perror("Read conversion");
-    exit(-1);
-  }
+        // read conversion register
+        if (read(fd, readBuf, 2) != 2) {
+                perror("Read conversion");
+                exit(-1);
+        }
 
-  float potVal = 0;
-  // multiply by 256 then add readBuf[1]
-  val = readBuf[0] << 8 | readBuf[1];
-  if (val < 0)   val = 0;  // LSB sometimes generates very low neg number.
+        float potVal = 0;
+        // multiply by 256 then add readBuf[1]
+        val = readBuf[0] << 8 | readBuf[1];
+        if (val < 0) val = 0;  // LSB sometimes generates very low neg number.
 
-  potVal = val * VPS;
-  return potVal;
+        potVal = val * VPS;
+        return potVal;
 }
 
 void read_n_values(int n){
-  float maxVolt = 2.0;
+        float maxVolt = 2.0;
 
-  for (int i = 0; i < n; ++i){
-    float curVolt = read_ads();
-    curVolt = std::round(curVolt * 100) / 100;
+        for (int i = 0; i < n; ++i) {
+                float curVolt = read_ads();
+                curVolt = std::round(curVolt * 100) / 100;
+                if (curVolt > maxVolt) maxVolt = curVolt;
+                float curValue = curVolt / maxVolt;
 
-    if (curVolt > maxVolt) maxVolt = curVolt;
-
-    float curValue = curVolt / maxVolt;
-
-    potis.setPoti(0, curValue);
-    cout << curVolt << " Volt => " << potis.getPoti(0) << endl;
-    usleep(10000);  // microseconds
-  }
+                potis.setPoti(0, curValue);
+                cout << curVolt << " Volt => " << potis.getPoti(0) << endl;
+                usleep(10000); // microseconds
+        }
 }
 
 void read_cont_values(){
-  float maxVolt = 2.0;
-  float lastVolt = 0.0;
+        float maxVolt = 2.0;
+        float lastVolt = 0.0;
 
-  for(;;){
-    float curVolt = read_ads();
-    curVolt = std::round(curVolt * 100) / 100;
+        for(;;) {
+                float curVolt = read_ads();
+                curVolt = std::round(curVolt * 100) / 100;
 
-    if (curVolt != lastVolt){
-      lastVolt = curVolt;
-      if (curVolt > maxVolt) maxVolt = curVolt;
+                if (curVolt != lastVolt) {
+                        lastVolt = curVolt;
+                        if (curVolt > maxVolt) maxVolt = curVolt;
+                        float curValue = curVolt / maxVolt;
 
-      float curValue = curVolt / maxVolt;
-
-      potis.setPoti(0, curValue);
-      cout << curVolt << " Volt => " << potis.getPoti(0) << endl;
-      usleep(10000);  // microseconds
-    }
-  }
+                        potis.setPoti(0, curValue);
+                        cout << curVolt << " Volt => " << potis.getPoti(0) << endl;
+                        usleep(10000); // microseconds
+                }
+        }
 }
 
 
 
 float Potis::getPoti(int potNo){
-  float potVal = 0;
-  switch (potNo) {
-    case 0: potVal = poti0; break;
-    case 1: potVal = poti1; break;
-    case 2: potVal = poti2; break;
-    case 3: potVal = poti3; break;
-    default: std::cout<< "Provide poti ID!" << std::endl;
-  }
-  return potVal;
+        float potVal = 0;
+        switch (potNo) {
+        case 0: potVal = poti0; break;
+        case 1: potVal = poti1; break;
+        case 2: potVal = poti2; break;
+        case 3: potVal = poti3; break;
+        default: std::cout<< "Provide poti ID!" << std::endl;
+        }
+        return potVal;
 }
 
 void Potis::setPoti(int potNo, float potVal){
-  switch (potNo) {
-    case 0: poti0 = potVal; break;
-    case 1: poti1 = potVal; break;
-    case 2: poti2 = potVal; break;
-    case 3: poti3 = potVal; break;
-    default: std::cout<< "Provide poti ID!" << std::endl;
-  }
+        switch (potNo) {
+        case 0: poti0 = potVal; break;
+        case 1: poti1 = potVal; break;
+        case 2: poti2 = potVal; break;
+        case 3: poti3 = potVal; break;
+        default: std::cout<< "Provide poti ID!" << std::endl;
+        }
 }
 
--- src/wdfLooper.cpp
+++ src/wdfLooper.cpp
@@ -29,25 +29,26 @@ wdfTree *myWdfTree;
 
 public:
 
+// Apply WDF Filter
 static void wdf_callback(wdfTree *myWdfTree, jack_nframes_t nframes, audioBufVector outBufsWdf){
         for (unsigned int i_sample = 0; i_sample < nframes; i_sample++)
         {
-                //float inVoltage = outBufs[0][i_sample];
                 myWdfTree->setInputValue(outBufsWdf[0][i_sample]);
                 myWdfTree->cycleWave();
                 outBufsWdf[0][i_sample] = { (float)(myWdfTree->getOutputValue()) };
         }
 }
 
+// Adapt Filter Parameters by Poti
 static void wdf_adaptParam(wdfTree *myWdfTree){
         extern Potis potis;
         float P0_s = 0;
 
-        for (;;){
-          if (P0_s != potis.getPoti(0)){
-            P0_s = potis.getPoti(0);
-            myWdfTree->setParam(2, P0_s);
-          }
+        for (;;) {
+                if (P0_s != potis.getPoti(0)) {
+                        P0_s = potis.getPoti(0);
+                        myWdfTree->setParam(2, P0_s);
+                }
         }
 }
 
@@ -60,26 +61,24 @@ virtual int audioCallback(jack_nframes_t nframes,
 
         std::thread wdfThread(SampleLooper::wdf_callback, myWdfTree, nframes, outBufs);
 
-        //myWdfTree->setParam(2, 6);
-        /// LOOP over all output buffers
+        // LOOP over all output buffers
         for(unsigned int i = 0; i < 1; i++)
         {
                 sample->get_frame(nframes, outBufs[0]);
-
-                wdfThread.join();
+                wdfThread.join();  // join with audioCallback
 
         }
         return 0;
 }
 
-/// Constructor
+// Constructor
 SampleLooper(std::string fileName, float playbackRate) :
         JackCpp::AudioIO("sample_looper", 0,1){
 
         reserveInPorts(2);
         reserveOutPorts(2);
 
-        /// allocate the sample player
+        // allocate the sample player
         sample = new SingleSample(fileName);
         sample->set_rate(playbackRate);
 
@@ -88,6 +87,7 @@ SampleLooper(std::string fileName, float playbackRate) :
         myWdfTree->setSamplerate(44100);
         myWdfTree->adaptTree();
 
+        // start adapt filter thread
         std::thread wdf_adaptParam_thread(SampleLooper::wdf_adaptParam, myWdfTree);
         wdf_adaptParam_thread.detach();
 
@@ -97,40 +97,37 @@ SampleLooper(std::string fileName, float playbackRate) :
 
 /// MAIN
 int main(int argc, char *argv[]){
-        // WDF stuff
-        //wdfTree *myWdfTree;
-
-        float playbackRate = 2.0;
 
         if(argc<=1) {
                 cout << "Pass path to wav-file as argument!" << endl;
                 return 0;
         }
 
-        /// Create looper with filename from first commandline arg
-        SampleLooper * t = new SampleLooper( std::string(argv[1]), playbackRate );
+        // Create looper with filename from first commandline arg
+        SampleLooper * t = new SampleLooper( std::string(argv[1]), 2.0 );
 
-        /// Start the jack client (also starts loop)
+        // Start the jack client (also starts loop)
         t->start();
 
-        /// connect mono loop port to stereo output ports
+        // Start ADS and Potis
+        setup_ads();
+        std::thread readingThread(read_cont_values);
+        readingThread.detach();
+        
+        // connect mono loop port to stereo output ports
         t->connectToPhysical(0,0);
         t->connectToPhysical(0,1);
 
-        ///print names
+        // Print Output ports
         cout << "Output port(s):" << endl;
         for(unsigned int i = 0; i < t->outPorts(); i++)
                 cout << "\t" << t->getOutputPortName(i) << endl;
 
-        /// Potis
-        setup_ads();
-        std::thread readingThread(read_cont_values);
-        readingThread.detach();
-        /// Run until ENTER key
+        // Run until ENTER key
         printf("### Press ENTER to quit ###\n");
         getchar();
 
-        /// Break down
+        // Break down
         t->disconnectOutPort(0); // Discconect out port.
         t->close(); // Stop client.
         delete t; // Always clean up after yourself.
